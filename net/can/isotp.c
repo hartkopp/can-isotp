a5f0023949e93542b1a6b910daf66ce24427f1dd
Oliver Hartkopp <socketcan@hartkopp.net>
2020-01-15 20:10:12 +0100

    can-isotp: introduce CAN_REQUIRED_SIZE macro
    Following upstream commit:
    9868b5d44f3 ("can: introduce CAN_REQUIRED_SIZE macro")
    
    Requires Linux 5.4+
    
    Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>

--- net/can/isotp.c
+++ net/can/isotp.c
@@ -1053,7 +1053,7 @@ static int isotp_bind(struct socket *sock, struct sockaddr *uaddr, int len)
 	int err = 0;
 	int notify_enetdown = 0;
 
-	if (len < sizeof(*addr))
+	if (len < CAN_REQUIRED_SIZE(*addr, can_ifindex))
 		return -EINVAL;
 
 	if (addr->can_addr.tp.rx_id == addr->can_addr.tp.tx_id)
